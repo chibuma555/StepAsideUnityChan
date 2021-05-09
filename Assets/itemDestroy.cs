659de4a5028d5293ce6d549debf8e4f56f255faf
chibuma555 <82083675+chibuma555@users.noreply.github.com>
2021-05-09 21:00:27 +0900

    Initial commit

---
+++ Assets/itemDestroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemDestroy : MonoBehaviour
+{
+    // unityちゃんを入れる
+    private GameObject unitychan;
+
+    private float length;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // unityちゃんと自身の距離(z座標）
+        this.length = this.transform.position.z - unitychan.transform.position.z;
+        // 距離に応じてオブジェクトを破棄する
+        if (length <= -5.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
